15f06fc5bd696753e52e5da8012cf69ac8f2e393
Raunakkumar012 <raunakkumarsingh0473@gmail.com>
2025-06-27 16:15:12 +0530

    Add files via upload

---
+++ task4.sql
@@ -0,0 +1,19 @@
+SELECT 
+  SUM(quantity) AS total_quantity_sold,
+  AVG(price) AS average_price,
+  COUNT(*) AS number_of_sales
+FROM sales;
+SELECT 
+  category,
+  SUM(quantity) AS total_quantity,
+  AVG(price) AS avg_price,
+  COUNT(*) AS sale_count
+FROM sales
+GROUP BY category;
+SELECT 
+  category,
+  SUM(quantity) AS total_quantity,
+  AVG(price) AS avg_price
+FROM sales
+GROUP BY category
+HAVING SUM(quantity) > 5;
